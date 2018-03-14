e7178c0e3c3a109f875faec13290a2eddc915e6c
Felix <Felix@LAPTOP-9SKBO2EE.fritz.box>
2018-03-14 13:52:21 +0100

    JDBC soweit bis auf die Daten der Datenbank selbst fertig. (non tested)

--- finDB/src/bp/jdbc/Connection.java
+++ finDB/src/bp/jdbc/Connection.java
@@ -1,5 +1,42 @@
 package bp.jdbc;
 
-public class Connection {
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class Connection {
+	
+	static final String JDBC_Driver = "com.mysql.jdbc.Driver"; 
+	static final String DB_URL = "";
+	
+	static final String User = ""; 
+	static final String Pass = ""; 
+	
+	public static void main (String[] args) {
+		Connection conn = null;
+		Statement stmt = null;
+		
+		try {
+//			Treiber initialisieren.
+			Class.forName("com.mysql.jdbc.Driver");
+			
+//			Verbindung laden.
+			System.out.println("Verbindung wird aufgebaut...");
+			conn = (Connection) DriverManager.getConnection(DB_URL, User, Pass);
+			
+//			Statement/Query ausführen
+			System.out.println("Query wird übermittelt.");
+			stmt = ((java.sql.Connection) conn).createStatement();
+			String sql;
+			sql = "SELECT * FROM TABLE";
+			ResultSet rs = stmt.executeQuery(sql);
+		} 
+
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
