312ef71b3d998241fbaf85dfd8f4a7b72e4bec51
haya <haya@DESKTOP-1QQ3DA5>
2018-03-15 10:22:54 +0100

    changed TickerController and added YahooController

--- finDB/src/bp/Controller/YahooController.java
+++ finDB/src/bp/Controller/YahooController.java
@@ -1,5 +1,27 @@
 package bp.Controller;
 
-public class YahooController {
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
+import bp.Model.Ticker;
+
+public class YahooController {
+	private ArrayList<URL> urlList;
+	
+	public URL getURLfromTicker(Ticker ticker){
+		this.urlList = new ArrayList<URL>();
+		TickerController tickerController = new TickerController();
+		ArrayList<Ticker> tickerList = tickerController.getTickerList();
+		for (Ticker tick: tickerList){
+			try {
+				URL u = new URL("https://de.finance.yahoo.com/quote/" + tick.getSymbol() + "?p=" + tick.getSymbol());
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+		}
+			
+		return null;
+		
+	}
 }
