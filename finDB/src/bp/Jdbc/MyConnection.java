db319379091f83e2e183329ceff2fb427425c33a
Felix <Felix@195.37.117.175>
2018-03-21 11:08:41 +0100

    # WARNING: head commit changed in the meantime
    New Connection Class immplemented.

---
+++ finDB/src/bp/Jdbc/MyConnection.java
@@ -0,0 +1,44 @@
+package bp.Jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+public class MyConnection {
+	
+	static final String JDBC_Driver = "com.mysql.jdbc.Driver"; 
+	static final String DB_URL = "";
+	
+	static final String User = ""; 
+	static final String Pass = ""; 
+	
+	public void connect() {
+		Connection conn = null;
+		Statement stmt = null;
+		
+		try {
+//			Treiber initialisieren.
+			Class.forName("com.mysql.jdbc.Driver");
+			
+//			Verbindung laden.
+			System.out.println("Verbindung wird aufgebaut...");
+			conn = DriverManager.getConnection(DB_URL, User, Pass);
+			
+//			Statement/Query ausführen
+			System.out.println("Query wird übermittelt.");
+			stmt = ((java.sql.Connection) conn).createStatement();
+			String sql;
+			sql = "SELECT * FROM TABLE";
+			ResultSet rs = stmt.executeQuery(sql);
+		} 
+
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
