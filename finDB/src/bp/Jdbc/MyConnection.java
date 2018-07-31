a6049a7cd238eddf8674a4f7b0cf4bcb2b39236c
FelixB1337 <36855407+FelixB1337@users.noreply.github.com>
2018-07-31 11:23:13 +0200

    Update MyConnection.java

--- finDB/src/bp/Jdbc/MyConnection.java
+++ finDB/src/bp/Jdbc/MyConnection.java
@@ -10,12 +10,13 @@ import java.sql.Statement;
 public class MyConnection {
 	
 	static final String JDBC_Driver = "com.mysql.jdbc.Driver"; 
-	static final String DB_URL = "";
+	static final String DB_URL = "jdbc:mysql://localhost/testba";
 	
-	static final String User = ""; 
+	static final String User = "root"; 
 	static final String Pass = ""; 
 	
-	public void connect() {
+//	public static void main (String[] args) {
+	public static void connection() {
 		Connection conn = null;
 		Statement stmt = null;
 		
@@ -26,13 +27,38 @@ public class MyConnection {
 //			Verbindung laden.
 			System.out.println("Verbindung wird aufgebaut...");
 			conn = DriverManager.getConnection(DB_URL, User, Pass);
+						
+// 			Auslesen der .csv Dateien und erstellen der Insert- und Create-Befehle
+			String QueryReader = "LOAD DATA LOCAL INFILE '" + "C:/Users/Felix/Downloads/AAPL.csv" + "' INTO TABLE aapltest FIELDS TERMINATED BY ','"
+					+ " LINES TERMINATED BY '\n' (`Date`, `Open`, `High`, `Low`, `Close`, `Adj Close`, `Volume`) ";
+//			Query aus PHP-MyAdmin:
+//			INSERT INTO `aapltest` (`Date`, `Open`, `High`, `Low`, `Close`, `Adj Close`, `Volume`) VALUES ('', '', '', '', '', '', '')
 			
-//			Statement/Query ausführen
-			System.out.println("Query wird übermittelt.");
-			stmt = ((java.sql.Connection) conn).createStatement();
-			String sql;
-			sql = "SELECT * FROM TABLE";
-			ResultSet rs = stmt.executeQuery(sql);
+			stmt = conn.createStatement();
+			stmt.execute(QueryReader);
+			
+//			Statement/Query ausfÃ¼hren
+//			System.out.println("Query wird Ã¼bermittelt.");
+//			stmt = ((java.sql.Connection) conn).createStatement();
+//			String sql;
+//			sql = "SELECT * FROM testba";
+//			ResultSet rs = stmt.executeQuery(sql);
+			
+// 			Daten auslesen und anzeigen
+//			while (rs.next()) {
+//				int Ident = rs.getInt("Ident");
+//				String Content = rs.getString("Content");
+//				
+//				System.out.println("Ident: " + Ident);
+//				System.out.println("Content: " + Content);
+//				System.out.println();
+//			}
+			
+// 			"AufrÃ¤umen"
+//			rs.close();
+			stmt.close();
+			conn.close();
+			System.out.println("Done!");
 		} 
 
 		catch (Exception e) {
@@ -41,4 +67,5 @@ public class MyConnection {
 		}
 		
 	}
+	
 }
