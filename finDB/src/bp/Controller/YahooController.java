2a309277596d9c781263f5f77be06394e4bd4a6d
tehhaya <hjanssen1@studenten.hs-bremerhaven.de>
2018-07-31 11:19:04 +0200

    Removed Interface

--- finDB/src/bp/Controller/YahooController.java
+++ finDB/src/bp/Controller/YahooController.java
@@ -14,7 +14,7 @@ import javax.net.ssl.HttpsURLConnection;
 
 import bp.Model.Ticker;
 
-public class YahooController implements IDownload {
+public class YahooController{
 	public TickerController tc;
 	private HttpsURLConnection conn;
 
