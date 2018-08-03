b069fab9ecf4e834c85651108a6d96c9c78e8220
haya <haya@172.22.18.184>
2018-08-03 12:31:11 +0200

    1257

---
+++ finDB/src/bp/test/test.java
@@ -0,0 +1,13 @@
+package bp.test;
+
+import bp.Controller.TickerController;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TickerController tc = new TickerController();
+		tc.download();
+	}
+
+}
