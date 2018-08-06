7c95cae78cb384065218750a86b69c2e130850a1
FelixB1337 <36855407+FelixB1337@users.noreply.github.com>
2018-08-06 13:45:25 +0200

    Update MainController.java
    TickerList TL als ArrayList.

--- finDB/src/bp/Controller/MainController.java
+++ finDB/src/bp/Controller/MainController.java
@@ -1,6 +1,13 @@
 package bp.Controller;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import bp.Jdbc.H2Connection;
+import bp.Model.Ticker;
 
 public class MainController {
 
@@ -11,5 +18,34 @@ public class MainController {
 		h2.connect();
 		h2.connectCreateTables(tc.getTickerList());
 		h2.disconnect();
-	}	
+		ArrayList<Ticker> TL = new ArrayList<Ticker>();
+		String csvFile = "C:\\Users\\Felix\\git\\bachelor_projekt\\finDB\\AllTickerSymbols";
+		BufferedReader br = null;
+		String line = "";
+
+		try {
+			br = new BufferedReader(new FileReader(csvFile));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			while (br.readLine() != null) {
+				try {
+					TL.add(new Ticker(br.readLine()));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for(Ticker ticker : TL) {
+			System.out.println(ticker.getSymbol());
+		}
+	}
 }
